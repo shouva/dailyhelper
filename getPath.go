033fba92ec58ee31e369aff5ad48fcfe2eec995c
Muhammad Bukhori Muslim <mosleim@gmail.com>
2019-05-05 17:41:09 +0700

    Adding function GetCurrentPath and GetConfig

---
+++ getPath.go
@@ -0,0 +1,31 @@
+package dailyhelper
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// GetPath :
+func GetPath(isbuild bool) string {
+	if isbuild {
+		return getExePath()
+	}
+	return getSourcePath()
+}
+func getSourcePath() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return pwd
+}
+func getExePath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	return exPath
+}
---
+++ helper.go
@@ -0,0 +1,33 @@
+package dailyhelper
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func readFile(path string) ([]byte, error) {
+	filesource, err := os.Open(path)
+	if err != nil {
+		fmt.Println("convert jsonFile ke byteArray")
+		panic(err)
+	}
+	defer filesource.Close()
+	return ioutil.ReadAll(filesource)
+}
+
+// ReadConfig :
+func ReadConfig(path string, config interface{}) error {
+	bytefile, err := readFile(path)
+	if err != nil {
+		fmt.Println("Convert jsonFile ke byteArray gagal")
+		panic(err)
+	}
+	err = json.Unmarshal(bytefile, &config)
+	if err != nil {
+		fmt.Println("Unmarshal config file gagal")
+		return err
+	}
+	return nil
+}
